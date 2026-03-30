19b4ae13af775a8a3fdf90317d7eff5b9ca6455a
karthiksuryaarasan <karthiksuryaarasan10@gmail.com>
2026-03-30 12:48:26 +0530

    Convert ETL to PySpark

--- src/etl.py
+++ src/etl.py
@@ -1,33 +1,55 @@
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import col
 from sqlalchemy import create_engine
 import pandas as pd
 
+# Create Spark Session
+spark = SparkSession.builder \
+    .appName("Sales ETL Pipeline") \
+    .getOrCreate()
+
+
 def extract():
-    df = pd.read_csv("data/sales.csv")
+    df = spark.read.csv(
+        "data/sales.csv",
+        header=True,
+        inferSchema=True
+    )
     return df
+
 
 def transform(df):
-    df['amount'] = df['amount'].astype(float)
-    df['order_date'] = pd.to_datetime(df['order_date'])
-    df = df.drop_duplicates()
+    df = df.dropDuplicates()
+
+    df = df.filter(col("amount") > 0)
+
     return df
 
+
 def load(df):
+
+    # Convert Spark → Pandas (for PostgreSQL load)
+    pandas_df = df.toPandas()
+
     db_url = "postgresql://postgres:postgres@localhost:5432/sales_db"
-    
+
     engine = create_engine(db_url)
 
-    df.to_sql(
+    pandas_df.to_sql(
         "sales",
         engine,
         if_exists="replace",
         index=False
     )
 
+
 def run():
     df = extract()
     df = transform(df)
     load(df)
-    print("ETL Completed Successfully")
+
+    print("PySpark ETL Completed Successfully")
+
 
 if __name__ == "__main__":
     run()
